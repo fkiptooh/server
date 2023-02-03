f558c4dab34589828b3e6717fbb99e29982b56a9
fkiptooh <fkiptooh@gmail.com>
2023-02-03 16:18:59 +0300

    Added admin routes with categories crud functionalities and other public routes

---
+++ controllers/category.js
@@ -0,0 +1,43 @@
+const Category = require('../models/category');
+const slugify = require('slugify');
+
+exports.create = async(req, res) => {
+    try{
+        const {name} = req.body;
+        const category = await new Category({name, slug: slugify(name)}).save();
+        res.json(category);
+    } catch(err){
+        console.log(err);
+        res.status(400).send("Category failed to create.");
+    }
+
+};
+
+exports.list = async(req, res) => 
+    res.json(await Category.find().sort({createdAt: -1}).exec());
+
+exports.remove = async(req, res) => {
+    try{
+        const deleted = await Category.findOneAndDelete({slug: req.params.slug});
+        res.json(deleted);
+    }
+    catch(err){
+        res.status(400).send('Failed to delete category');
+    }
+    
+};
+
+exports.update = async(req, res) => {
+    const {name} = req.body;
+    try{
+        const updated = await Category.findOneAndUpdate({slug: req.params.slug}, {name, slug: slugify(name)}, {new: true});
+        res.json(updated);
+    }catch(err){
+        res.status(400).send("Category update failed");
+    }  
+};
+
+exports.read = async(req, res) => {
+    let category = await Category.findOne({slug: req.params.slug}).exec();
+    res.json(category);
+};
---
+++ routes/category.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controller
+const { create, read, update, remove, list } = require("../controllers/category");
+// const { route } = require("./auth");
+
+//routes
+
+router.post("/category", authCheck, adminCheck, create);
+router.get("/categories", list);
+router.get("/category/:slug", read);
+router.put("/category/:slug", authCheck, adminCheck, update);
+router.delete("/category/:slug", authCheck, adminCheck, remove);
+
+
+
+module.exports = router;
