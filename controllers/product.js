c2ac3dbc191178d780e2300ce1f0498a8f989ee4
fkiptooh <fkiptooh@gmail.com>
2023-02-12 21:56:24 +0300

    Added list route to product route for displaying new arrivals and best sellers in product section in the products component

--- controllers/product.js
+++ controllers/product.js
@@ -65,3 +65,22 @@ exports.update = async (req, res) => {
     });
   }
 };
+
+exports.list = async (req, res)=> {
+  try {
+    // createdAt, updatedAt, desc/asc 3..
+    const { sort, order, limit } = req.body;
+    const products = await Product.find({})
+    .populate('categoty')
+    .populate('subcategory')
+    .sort([[sort, order]])
+    .limit(limit)
+    .exec();
+
+    res.json(products);
+
+  }
+  catch(err) {
+    console.log(err);
+  }
+}
