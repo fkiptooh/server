e3ecb768d603ec1b416e6ef96a2f9966a38cae29
fkiptooh <fkiptooh@gmail.com>
2023-02-17 08:14:33 +0300

    Added support for checking on related products on the backed through a select query statement

--- controllers/product.js
+++ controllers/product.js
@@ -152,3 +152,19 @@ exports.productStar = async(req, res) => {
   } 
 }
 
+exports.listRelated = async (req, res) => {
+  const product = await Product.findById(req.params.productId).exec();
+
+  const related = await Product.find({
+    _id: { $ne: product._id },
+    category: product.category,
+  })
+    .limit(3)
+    .populate("category")
+    .populate("subcategory")
+    .populate("ratings.postedBy")
+    .exec();
+
+  res.json(related);
+};
+
--- routes/product.js
+++ routes/product.js
@@ -14,6 +14,7 @@ const {
   list,
   productsCount,
   productStar,
+  listRelated,
 } = require("../controllers/product");
 
 // routes
@@ -27,5 +28,8 @@ router.post("/products", list)
 
 // star rating route
 router.put("/product/star/:productId", authCheck, productStar);
+
+// related
+router.get("/product/related/:productId", listRelated);
 
 module.exports = router;
