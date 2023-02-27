cb25bede9eda3e1f6f402f9cbe3c8588be6b30df
fkiptooh <fkiptooh@gmail.com>
2023-02-27 11:18:55 +0300

    Succefuly applied billing based on coupon via stripe payment

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -6,15 +6,32 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async(req, res)=> {
     // later apply coupon
+    const {couponApplied} = req.body;
     // later calculate price
+    // find user
+    const user = await User.findOne({email: req.user.email}).exec();
+    // get user cart total
+    const {cartTotal, totalAfterDiscount} =await Cart.findOne({orderedBy: user._id}).exec();
 
+    let finalAmount = 0;
+    if(couponApplied && totalAfterDiscount){
+        finalAmount = totalAfterDiscount;
+    } else {
+        finalAmount = cartTotal;
+    }
+
+    console.log(`CART TOTAL`, cartTotal);
+    // create payment intent with order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 30000,
+        amount: finalAmount * 100,
         currency: "KES",
     });
     
 
     res.send({
         clientSecret: paymentIntent.client_secret,
+        cartTotal,
+        totalAfterDiscount,
+        payable: finalAmount,
     })
 }
--- controllers/user.js
+++ controllers/user.js
@@ -108,7 +108,7 @@ exports.applyCouponToUserCart=async(req, res)=> {
     {orderedBy: user._id},
     {totalAfterDiscount},
     {new: true},
-    );
+    ).exec();
 
     res.json(totalAfterDiscount);
 }
